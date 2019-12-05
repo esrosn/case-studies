631aba941c8825bf71236bd77528397ff8b14315
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-12-05 17:20:08 -0600

    Add components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import './App.css'
+import { HomePage } from './pages'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route />
+        <Route exact path='/' component={HomePage} />
         <Route />
         <Route />
         <Route />
